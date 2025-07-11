59ced057253810f52271d6c7a2557b0909d437c9
unknown <yunusbraq@gmail.com>
2025-07-11 14:18:26 -0700

    new commit

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+
+import React from 'react'
+ // Make sure to replace with the correct path to your image
+
+function Home() {
+  return (
+    
+    <>
+    <div className=" bg-gray-800  items-center">
+        <div className="flex justify-center items-center min-h-screen gap-20 hidden sm:flex">
+            <div>
+                <p className="text-white font-bold text-xl">Hello</p>
+                <h1 className="text-4xl text-white font-bold">I am <span className="text-purple-700">Muhammad Younus</span>   </h1>
+                <p className="text-white font-bold text-xl">A Full Stack Developer</p>
+            </div>
+            {/* image */}
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/122626646?v=4" alt="Profile" className="w-48 h-48 rounded-full mt-4" />
+            </div>
+        </div>
+        <div className="flexitems-center  min-h-screen sm:hidden flex-col gap-10 p-6">
+            <div>
+                <p className="text-white font-bold text-xl">Hello</p>
+                <h1 className="text-2xl text-white font-bold">I am <span className="text-purple-700">Muhammad Younus</span>   </h1>
+                <p className="text-white font-bold text-xl">A Full Stack Developer</p>
+            </div>
+            {/* image */}
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/122626646?v=4" alt="Profile" className="w-48 h-48 rounded-full mt-4" />
+            </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+  <nav className="bg-purple-700 p-2">
+    <div className="h-12 p-2 flex justify-between items-center ">
+      <div className='text-white font-bold'>Portfolio</div>
+      <ul className="flex text-white font-bold gap-6 hidden sm:flex px-6 ">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Project</Link>
+        </li>
+        <li>
+          <Link to="/education">Education</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+      </ul>
+
+     
+
+    </div>
+
+     <ul className=" text-white font-semi-bold bg-gray-600  w-full gap-4  sm:hidden p-2  space-y-1">
+        
+         <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/education">Education</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+      </ul>
+
+  </nav>
+  );
+}
+
+export default Navbar;
