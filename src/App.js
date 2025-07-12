7c14a473135ee8342db52b359956bc33201a46b4
muh-younus <braq369@gmail.com>
2025-07-12 04:11:13 -0700

    Fix email for contribution graph

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Home from './components/Home'
 import About from './components/About'
 import Proj from './components/Proj'
 import Skill from './components/Skill'
-import Education from './components/Skill' // Assuming Education is also in Skill.js, adjust if needed
+import Education from './components/Skill' 
+import Contact from './components/Contact'// Assuming Education is also in Skill.js, adjust if needed
 
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/proj" element={<Proj />} />
           <Route path="/skills" element={<Skill />} />
-          <Route path="/education" element={<Skill />} /> 
+          <Route Route path="/Contact" element={<Contact />} />
         </Routes>
        
       </>
